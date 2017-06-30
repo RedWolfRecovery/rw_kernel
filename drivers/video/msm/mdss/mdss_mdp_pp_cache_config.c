5d4bb3efda318b300de992a7e0809b8adca52436
Savoca <savoca@codefi.re>
2017-06-30 20:31:44 +0800

    Add KCAL module
    msm: mdss: KCAL: disable igc update
    
    mdss: mdp: kcal for mdss_mdp_v1_7
    Adding the kcal interface by @savoca and
    @faux123
    porting from m9 version by @flar2
    
    Using the MDP 1_7 userspace structs to inject
    configurations of kcal to the pp_cache configs.
    Overriding userspace copy with memcpy when
    copy_from_user fails (in case of kcal interface
    used).
    
    Adding a simple mdp ctl struct caching to
    the kcal driver to grab ctl struct for config
    calls.
    
    Could be done nicer with direct interfacing
    to userspace, leaves some space to make the kernel
    crash if a third party app sends faulty configs,
    but there's no security issue or degradation
    of functionality with this memcpy patch.
    
    @tbalden
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    msm: mdss: kcal: Only apply updates on index 0
    This is just for qualities sake, all of the other functions around
    MDSS do this as well.
    
    msm: mdss: kcal: Use PCC for negative display mode
    
    msm: mdss: KCAL: Send a pp display commit when changes are made
    Sending a PP display commit when changes are made, to prevent waiting on a panel update from
    some other part of the kernel. Sending a display commit takes care of locking and panel state
    checking for us.
    
    QC MDSS also handles queue-ing already for us, so remove that functionality from KCAL.
    
    msm: mdss: KCAL: Do not read from picture adjustment registers
    - These registers are not reliable for reading at the moment.
      They will sometimes report a value offset by 0x100, and in some cases
      report 0, even with MDP_PP_OPS_ENABLE flagged.
    
    - Read from stored values for now
    
    msm: mdss: KCAL: Read values from MDP registers wherever possible
     - This will keep KCAL values updated even when other drivers write
       to MDP registers (LiveDisplay/Sony)
     - Qcom/CAF code does not support kernel-space readings of IGC lut yet
     - Adjust minimum PCC value from '0' to '1'
     - Bring kcal_min filter back to its own function, values may need to be
       updated but not written to MDP registers.
       ex: changes queued when display is off
     - Update IGC variables
    
    msm: mdss: KCAL: Apply default values on boot
     - This is to guarantee that our default values are the ones active
     - Use devm_kzalloc as it guarantees to free memory for us on device remove
     - Properly remove notifier on probe fail
    
    msm: mdss: KCAL: Queue changes when panel is powered off
     - Support MMI panel notifications and fb notifications by default
     - Properly kfree() when probe fails
     - Check RGB values against kcal_min in mdss_mdp_kcal_update_pcc()
    
    msm: mdss: KCAL: Remove MODULE_DESCRIPTION
     - Unnecessary
    
    msm: mdss: KCAL: Update Kconfig description
    
    msm: mdss: KCAL: Update according to linux guidelines and checkpatch.pl
     - Long overdue...
    
    msm: mdss: KCAL: Check panel power state before applying values
    
    msm: mdss: KCAL: Allow kcal_enable to control all post-processing fea…
    …tures
    
     - Also update function calls to accurately reflect driver
    
    msm: mdss: Keep KCAL within its own source file
     - Generate new IGC structure instead of pulling the current one
     - Remove unused header file
    
    msm: mdss: Remove deprecated __dev* markings
     - These probably shouldn't have even been here in the first place... Oops
     - https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=54b956b903607
    
    msm: mdss: Add pa_v2 support to KCAL
     - Thank you engstk for testing
    
    msm: mdss: Replace PGC implementation with PCC for KCAL
     - Unify enable/update routines
     - Remove no longer used update call from probe function
     - Pass lut_data struct to all kcal/mdss functions
     - Thank you to cyanogen for PCC code reference
    
    msm: mdss: Add KCAL support for post processing control [v2]

--- drivers/video/msm/mdss/mdss_mdp_pp_cache_config.c
+++ drivers/video/msm/mdss/mdss_mdp_pp_cache_config.c
@@ -671,9 +671,14 @@ static int pp_pcc_cache_params_v1_7(struct mdp_pcc_cfg_data *config,
 			(void *) v17_cache_data;
 		if (copy_from_user(&v17_usr_config, config->cfg_payload,
 				   sizeof(v17_usr_config))) {
+#ifdef CONFIG_FB_MSM_MDSS_KCAL_CTRL
+			memcpy(&v17_usr_config, config->cfg_payload, sizeof(v17_usr_config));
+			ret = 0;
+#else
 			pr_err("failed to copy v17 pcc\n");
 			ret = -EFAULT;
 			goto pcc_config_exit;
+#endif
 		}
 		if ((config->ops & MDP_PP_OPS_DISABLE)) {
 			pr_debug("disable pcc\n");
